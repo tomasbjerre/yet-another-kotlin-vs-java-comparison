a3423af5d66d6e3ba0cf1ae8cdd8e9a509765df5
Tomas Bjerre <tomas.bjerre85@gmail.com>
2018-08-20 15:19:10 +0200

    Update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-./gradlew build -i || exit
+./gradlew build -x verifyGoogleJavaFormat -i || exit
 find build/classes/kotlin/main/ | grep class$ | xargs java -jar lib/cfr_0_132.jar --outputdir src/main/java || exit
 ./gradlew generateComparison || exit
